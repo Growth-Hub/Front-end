4b1fcecdfcf6ad8df07b811e0f6f833706168785
kangsinbeom <kanian16@naver.com>
2024-02-27 16:15:42 +0900

    flx: fix style warning

--- course1_react/kangsinbeom/src/components/shared/Flex.tsx
+++ course1_react/kangsinbeom/src/components/shared/Flex.tsx
@@ -3,15 +3,15 @@ import styled from "styled-components";
 
 interface FlexProps {
   $align?: CSSProperties["alignItems"];
-  justify?: CSSProperties["justifyContent"];
-  direction?: CSSProperties["flexDirection"];
+  $justify?: CSSProperties["justifyContent"];
+  $direction?: CSSProperties["flexDirection"];
 }
 
-const Flex = styled.div<FlexProps>(({ $align, justify, direction }) => ({
+const Flex = styled.div<FlexProps>(({ $align, $justify, $direction }) => ({
   display: "flex",
   alignItems: $align,
-  justifyContent: justify,
-  flexDirection: direction,
+  justifyContent: $justify,
+  flexDirection: $direction,
 }));
 
 export default Flex;
--- course1_react/kangsinbeom/src/components/shared/ItemList.tsx
+++ course1_react/kangsinbeom/src/components/shared/ItemList.tsx
@@ -1,17 +1,30 @@
-import Item from "./Item";
-import Grid from "./Grid";
-import useGetDogs from "../../hooks/useGetDogs";
 import withSuspense from "../../hooks/withSuspense";
 import ListSkeleton from "./ListSkeleton";
+import { useInView } from "react-intersection-observer";
+import { useEffect, useState } from "react";
+import useInfinityGetDogs from "../../hooks/useInfinityGetDogs";
+import Grid from "./Grid";
+import Item from "./Item";
+import useSaveScroll from "../../hooks/useSaveScroll";
 
 const ItemList = () => {
-  const { data } = useGetDogs();
-
+  const { ref, inView } = useInView();
+  const { data, fetchNextPage } = useInfinityGetDogs();
+  useSaveScroll();
+  useEffect(() => {
+    if (inView) fetchNextPage();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inView]);
+  useEffect(() => {
+    const scrollY = sessionStorage.getItem("scrollY");
+    window.scrollTo({ top: JSON.parse(scrollY as string) as number });
+  }, []);
   return (
     <Grid $coulmn="repeat(5, 1fr)" style={{ gap: 20 }}>
-      {data.map(({ url, id }) => (
-        <Item url={url} key={id} />
+      {data.pages.flat().map(({ url, id, breeds }) => (
+        <Item url={url} key={id} breeds={breeds} />
       ))}
+      <div ref={ref}>나 보이면 inview</div>
     </Grid>
   );
 };
